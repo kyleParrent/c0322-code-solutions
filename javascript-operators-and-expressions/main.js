3598802b9479acfd990df9909bb55784a03b8952
Kyle Parrent <kppp433@gmail.com>
2022-03-21 19:34:29 +0000

    Completed javascript-operators-and-expressions.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,46 @@
+var width = 10;
+var height = 12;
+var area = width * height;
+
+console.log('value of area:', area);
+console.log('area typeof:', typeof area);
+
+var bill = 100;
+var payment = 375;
+var change = payment - bill;
+
+console.log('value of change:', change);
+console.log('change typeof:', typeof change);
+
+var quizzes = 95;
+var midterm = 90;
+var final = 85;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade:', grade);
+console.log('grade typeof:', typeof grade);
+
+var firstName = 'Kyle ';
+var lastName = 'Parrent';
+var fullName = firstName + lastName;
+
+console.log('value of fullName:', fullName);
+console.log('fullName typeof:', typeof fullName);
+
+var ph = 2;
+var isAcidic = ph < 7;
+
+console.log('value of isAcidic:', isAcidic);
+console.log('isAcidic typeof:', typeof isAcidic);
+
+var headCount = 50;
+var isSparta = headCount === 300;
+
+console.log('value of isSparta:', isSparta);
+console.log('isSparta typeof:', typeof isSparta);
+
+var motto = fullName;
+motto = motto + ' is the GOAT';
+
+console.log('value of motto:', motto);
+console.log('motto typeof:', typeof motto);
