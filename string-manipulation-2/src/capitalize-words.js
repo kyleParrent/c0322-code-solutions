dbf1d297b00a9857818eb0b2d33af5c400926c07
Kyle Parrent <kppp433@gmail.com>
2022-03-29 17:04:23 +0000

    Parts of string-manipulation-2.

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,1 +1,17 @@
 /* exported capitalizeWords */
+
+function capitalizeWords(string) {
+  var newS = string.splt(' ');
+  var newWord = '';
+  for (var e = 0; e < newS.length; e++) {
+    for (var i = 1; i < string.length; i++) {
+      newWord += newS[0].toUpperCase();
+      newWord += newS[i].toLowerCase();
+    }
+  }
+  return newWord;
+}
+
+// if its a string do one thing and if its a list of words then split
+// at the space and re add it to a new var and return  that
+// {needs, solution}
