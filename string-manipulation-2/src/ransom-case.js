dc3ed2b8066e6938c5da214207484e291358ecc9
Kyle Parrent <kppp433@gmail.com>
2022-03-31 23:55:55 +0000

    Completed string-manipulation-2.

--- string-manipulation-2/src/ransom-case.js
+++ string-manipulation-2/src/ransom-case.js
@@ -11,3 +11,13 @@ function ransomCase(string) {
   }
   return newWord;
 }
+
+// input: 'hELLo'
+// output: 'hElLo'
+
+// create a variable for the new string to be placed
+// make a for statement that goes to the end of the string
+// create an if statement that checks if the current index is divisible by 2 with no remainder
+// if true then make the letter toLowerCase and add it to the string
+// else make it upper case and add it to the string
+// at the end of the function return the string
