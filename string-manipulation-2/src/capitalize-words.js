7da4750d3b51bb2aae061c068cd7c39af4c5967a
Kyle Parrent <kppp433@gmail.com>
2022-03-31 00:25:36 +0000

    not completed string-manipulation-2.

--- string-manipulation-2/src/capitalize-words.js
+++ string-manipulation-2/src/capitalize-words.js
@@ -1,19 +1,29 @@
 /* exported capitalizeWords */
 
 function capitalizeWords(string) {
-  var newS = string.splt(' ');
+  var newS = string.split(' ');
   var newWord = '';
   var newString = '';
   for (var e = 0; e < newS.length; e++) {
-    newWord = newS[e] + ' ';
+    newWord = newS[e];
     newString += newWord[0].toUpperCase();
-    for (var i = 1; i < newWord[e].length; i++) {
+    for (var i = 1; i < newWord.length; i++) {
       newString += newWord[i].toLowerCase();
+    }
+    for (var o = 1; o < newS.length; o++) {
+      newString += ' ';
     }
   }
   return newString;
 }
 
-// if its a string do one thing and if its a list of words then split
-// at the space and re add it to a new var and return  that
-// {needs, solution}
+// input: 'hello toM, whAt up'
+// output: 'Hello Tom, What Up'
+
+// take the string and spilt it up into pieces at the space
+// create a varible for the word to go
+// create a varible for the new captized string
+// create a for loop to grab each index in the new split array
+// change the first index on that word to caplictal
+// loop through the rest of the string making everything toLowerCase
+// push the new word to the new string
