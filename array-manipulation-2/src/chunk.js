dd54fc957bd22629b21ec19f015f40630ee921e1
Kyle Parrent <kppp433@gmail.com>
2022-04-01 18:46:58 +0000

    Last array-manipulation-2.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -3,15 +3,21 @@
 function chunk(array, size) {
   var newSmallArr = [];
   var newArr = [];
-  if (size > array.length) {
-    for (var i = 0; i < array.length; i++) {
-      newArr.push(array[i]);
+  var counter = 0;
+  for (var i = 0; i < array.length; i += size) {
+    counter += size;
+    if (counter > array.length) {
+      for (var t = size; t < array.length; t++) {
+        newSmallArr.push(array[t]);
+      }
+      return newArr;
+    } else {
+      for (var ii = i; ii < size + i; ii++) {
+        newSmallArr.push(array[ii]);
+      }
     }
-  } else {
-    for (var ii = 0; ii < size; ii++) {
-      newSmallArr.push(array[i]);
-    }
+    newArr.push(newSmallArr);
+    newSmallArr = [];
   }
-  newArr.push(newSmallArr);
-  newArr = [];
+  return newArr;
 }
