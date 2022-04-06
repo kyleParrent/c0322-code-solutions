29ee2dd85568d4cffb6a970a45b0aede9964de9e
Kyle Parrent <kppp433@gmail.com>
2022-04-06 17:02:53 +0000

    Partial pbject 2.

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,12 @@
 /* exported omit */
+
+function omit(source, keys) {
+  var i = 0;
+  for (var x in source) {
+    if (x === keys[i]) {
+      delete source[x];
+      i++;
+    }
+  }
+  return source;
+}
