efc5c5074813b3d630e63dd41dd281044cf26e72
Kyle Parrent <kppp433@gmail.com>
2022-04-11 20:09:10 +0000

    Part object-manipulation-2.

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,12 +1,16 @@
 /* exported omit */
 
 function omit(source, keys) {
-  var i = 0;
-  for (var x in source) {
-    if (x === keys[i]) {
-      delete source[x];
-      i++;
+  var newObj = {};
+  for (var i = 0; i < keys.length; i++) {
+    for (var x in source) {
+      if (x !== keys[i]) {
+        newObj[x] = source[x];
+      }
     }
   }
-  return source;
+  return newObj;
 }
+
+// can't figure how to keep keys previously cycled through from being addded. in the next compariosn. or since the
+// the compariosn will add all keys not currently being compared it will add the next key to be excluded when doing the first compariosn.
