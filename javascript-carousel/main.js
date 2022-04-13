41582a7ab3ed43b228f8062bc4917437ca221028
Kyle Parrent <kppp433@gmail.com>
2022-04-13 23:59:34 +0000

    Completed javascript-carousel.

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,22 +1,84 @@
 
 var rightArrow = document.querySelector('.right');
 var currentImage = document.querySelector('.current-image');
+var leftArrow = document.querySelector('.left');
+var dots = document.querySelector('.dots');
+var picIndex = ['images/001.png', 'images/004.png', 'images/007.png', 'images/025.png', 'images/039.png'];
+var intID = setInterval(intSwitch, 3000);
+var icon = document.querySelectorAll('.icon');
 
 function moveRight(event) {
-  var iconON = document.querySelector('.icon-on');
-  if (currentImage.getAttribute('src') === 'images/001.png') {
-    currentImage.setAttribute('src', 'images/004.png');
-    iconON.className = 'far fa-circle icon';
-    iconON.nextSibling.className = 'fas fa-circle icon-on';
-  } else if (currentImage.getAttribute('src') === 'images/004.png') {
-    currentImage.setAttribute('src', 'images/007.png');
-  } else if (currentImage.getAttribute('src') === 'images/007.png') {
-    currentImage.setAttribute('src', 'images/025.png');
-  } else if (currentImage.getAttribute('src') === 'images/025.png') {
-    currentImage.setAttribute('src', 'images/039.png');
-  } else if (currentImage.getAttribute('src') === 'images/039.png') {
-    currentImage.setAttribute('src', 'images/001.png');
+  clearInterval(intID);
+  intID = setInterval(intSwitch, 3000);
+  for (var i = 0; i < icon.length; i++) {
+    if (icon[i].getAttribute('class') === 'fas fa-circle icon') {
+      if (i === icon.length - 1) {
+        icon[i].className = 'far fa-circle icon';
+        icon[0].className = 'fas fa-circle icon';
+        currentImage.setAttribute('src', picIndex[0]);
+      } else {
+        icon[i].className = 'far fa-circle icon';
+        icon[i + 1].className = 'fas fa-circle icon';
+        currentImage.setAttribute('src', picIndex[i + 1]);
+        return;
+      }
+    }
   }
 }
 
 rightArrow.addEventListener('click', moveRight);
+
+function moveLeft(event) {
+  clearInterval(intID);
+  intID = setInterval(intSwitch, 3000);
+  for (var i = 4; i >= 0; i--) {
+    if (icon[i].getAttribute('class') === 'fas fa-circle icon') {
+      if (i === 0) {
+        icon[i].className = 'far fa-circle icon';
+        icon[4].className = 'fas fa-circle icon';
+        currentImage.setAttribute('src', picIndex[4]);
+      } else {
+        icon[i].className = 'far fa-circle icon';
+        icon[i - 1].className = 'fas fa-circle icon';
+        currentImage.setAttribute('src', picIndex[i - 1]);
+        return;
+      }
+    }
+  }
+}
+
+leftArrow.addEventListener('click', moveLeft);
+
+function dotSwitch(event) {
+  clearInterval(intID);
+  intID = setInterval(intSwitch, 3000);
+  if (event.target.matches('.icon')) {
+    for (var i = 0; i < icon.length; i++) {
+      if (event.target === icon[i]) {
+        icon[i].className = 'fas fa-circle icon';
+        currentImage.setAttribute('src', picIndex[i]);
+      } else {
+        icon[i].className = 'far fa-circle icon';
+      }
+    }
+  }
+}
+
+dots.addEventListener('click', dotSwitch);
+
+function intSwitch() {
+  for (var i = 0; i < icon.length; i++) {
+    if (icon[i].getAttribute('class') === 'fas fa-circle icon') {
+      if (i === icon.length - 1) {
+        icon[i].className = 'far fa-circle icon';
+        icon[0].className = 'fas fa-circle icon';
+        currentImage.setAttribute('src', picIndex[0]);
+      } else {
+        icon[i].className = 'far fa-circle icon';
+        icon[i + 1].className = 'fas fa-circle icon';
+        currentImage.setAttribute('src', picIndex[i + 1]);
+        return;
+      }
+    }
+  }
+}
