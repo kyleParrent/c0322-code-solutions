9342f2d7788ec8862e399eb4a16b1ebd2f024b1e
Kyle Parrent <kppp433@gmail.com>
2022-04-14 00:56:29 +0000

    Part string-manipulation-3.

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,19 @@
 /* exported reverseWords */
+
+function reverseWords(string) {
+  var newStrArr = string.split(' ');
+  var newStr = '';
+  for (var i = 0; i < newStrArr.length; i++) {
+    var newWord = '';
+    for (var y = newStrArr[i].length - 1; y >= 0; y--) {
+      var oldWord = newStrArr[i];
+      newWord += oldWord[y];
+    }
+    if (i !== newStrArr.length - 1) {
+      newStr += newWord + ' ';
+    } else {
+      newStr += newWord;
+    }
+  }
+  return newStr;
+}
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,27 @@
 /* exported titleCase */
+
+function titleCase(title) {
+  var newStrArr = title.split(' ');
+  var newStr = '';
+  for (var i = 0; i < newStrArr.length; i++) {
+    var newWord = '';
+    for (var y = 0; y < newStrArr[i].length; y++) {
+      var oldWord = newStrArr[i];
+      if (y === 0) {
+        newWord += oldWord[y].toUpperCase();
+      } else if (y - 1 === '-') {
+        newWord += oldWord[y].toUpperCase();
+      } else if (oldWord.length <= 3) {
+        newWord += oldWord[y].toLowerCase();
+      } else {
+        newWord += oldWord[y].toLowerCase();
+      }
+    }
+    if (i !== newStrArr.length - 1) {
+      newStr += newWord + ' ';
+    } else {
+      newStr += newWord;
+    }
+  }
+  return newStr;
+}
