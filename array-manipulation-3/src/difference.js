f6da74f9a159fee16239f1735d1d4c91bd048a4b
Kyle Parrent <kppp433@gmail.com>
2022-04-29 20:31:05 +0000

    Part.

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,6 +1,22 @@
 /* exported difference */
 
-// input
+function difference(first, second) {
+  var newArr = [];
+  for (var i = 0; i < first.length; i++) {
+    if (second.includes(first[i]) === false) {
+      newArr.push(first[i]);
+    }
+  }
+  for (var u = 0; u < second.length; u++) {
+    if (first.includes(second[u]) === false) {
+      newArr.push(second[u]);
+    }
+  }
+  return newArr;
+}
+
+// input [3,6,4,8,2] [0,4,8,9,1]
+// output [3,6,2,0,9,1]
 
 // create a new varible to store the unique values
 // create a loop that goes through the first given array
--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,16 @@
 /* exported union */
+
+function union(first, second) {
+  var newArr = [];
+  for (var i = 0; i < first.length; i++) {
+    if (newArr.includes(first[i]) === false) {
+      newArr.push(first[i]);
+    }
+  }
+  for (var u = 0; u < second.length; u++) {
+    if (newArr.includes(second[u]) === false) {
+      newArr.push(second[u]);
+    }
+  }
+  return newArr;
+}
