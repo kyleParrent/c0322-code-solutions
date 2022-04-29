ab2d9b9dff8e3f38e8e6272e15917bbe455b7ba9
Kyle Parrent <kppp433@gmail.com>
2022-04-29 18:06:43 +0000

    Part.

--- array-manipulation-3/src/unique.js
+++ array-manipulation-3/src/unique.js
@@ -1,1 +1,20 @@
 /* exported unique */
+
+function unique(array) {
+  var newArr = [];
+  for (var i = 0; i < array.length; i++) {
+    if (newArr.includes(array[i]) === false) {
+      newArr.push(array[i]);
+    }
+  }
+  return newArr;
+}
+
+// input: [the , word, hello, the, hello]
+// output: [the, word, hello]
+
+// create a varibel for array values
+// create a for loop that goes through the array
+// have an if statment that thecks if the value at the current index is inclued in the new array
+// if false then push the current value to the new array
+// once the for loop finishes return the new array from the funcction
