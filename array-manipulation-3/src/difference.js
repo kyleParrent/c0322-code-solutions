6f09b07cfd3d87679d6d736731b26371deb17725
Kyle Parrent <kppp433@gmail.com>
2022-04-29 19:31:09 +0000

    Part.

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,11 @@
 /* exported difference */
+
+// input
+
+// create a new varible to store the unique values
+// create a loop that goes through the first given array
+// create a condition that checks if the current index is inclued in the second array
+// if its not then push it to the new array for unique values
+// create a second loop that does the same thing except for the second arrays length and comparing
+// aginst the first in the condition
+// once both loops are completed return the new array from the function
