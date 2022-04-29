cea4066a8feb5f33a73b3c44a45161c4492bfe2f
Kyle Parrent <kppp433@gmail.com>
2022-04-29 21:57:22 +0000

    Completed arrays 3.

--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -14,3 +14,15 @@ function union(first, second) {
   }
   return newArr;
 }
+
+// input [4,6,7] [8,6,4]
+// output [4,6]
+
+// create a variable to put the new array values
+// create a for loop that goes the length of the first array
+// create a condition that checks to see if the new array contains the current index in first
+// if not then push the value to the new array
+// create another for loop to go the length of the second array
+// create a condition that checks to see if the new array contains the current index in second
+// if not then push the value to the new array
+// once the loops finish push the values to the new array
