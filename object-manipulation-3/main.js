3db9f02ce2f9478b37a9bf37351f46a6ea3f666c
Kyle Parrent <kppp433@gmail.com>
2022-04-30 00:25:56 +0000

    Almost?.

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -1,7 +1,7 @@
 console.log('Lodash is loaded:', typeof _ !== 'undefined');
 
 var players = [
-  { name: 'Obi Wan', hand: [] },
+  { name: 'ObiWan', hand: [] },
   { name: 'Anakin', hand: [] },
   { name: 'Grievous', hand: [] },
   { name: 'Droid', hand: [] }
@@ -61,3 +61,57 @@ var cards = [
   { rank: '2', suit: 'Clubs' },
   { rank: '2', suit: 'Spades' }
 ];
+
+var shuffCards = _.shuffle(cards);
+
+for (var i = 0; i < players.length; i++) {
+  players[i].hand = _.sampleSize(shuffCards, 2);
+  shuffCards = _.pullAll(shuffCards, players[i].hand);
+}
+
+var currentWinner = '';
+var winnerTotal = 0;
+var total = 0;
+
+for (var y = 0; y < players.length; y++) {
+  var currentPlayer = players[y];
+  var currentHand = currentPlayer.hand;
+  for (var u = 0; u < currentHand.length; u++) {
+    var currentCard = currentHand[u];
+    for (var x in currentCard) {
+      if (currentCard[x] === 'Ace') {
+        total += 11;
+      } else if (currentCard[x] === 'King') {
+        total += 10;
+      } else if (currentCard[x] === 'Queen') {
+        total += 10;
+      } else if (currentCard[x] === 'Jack') {
+        total += 10;
+      } else if (currentCard[x] === '10') {
+        total += 10;
+      } else if (currentCard[x] === '9') {
+        total += 9;
+      } else if (currentCard[x] === '8') {
+        total += 8;
+      } else if (currentCard[x] === '7') {
+        total += 7;
+      } else if (currentCard[x] === '6') {
+        total += 6;
+      } else if (currentCard[x] === '5') {
+        total += 5;
+      } else if (currentCard[x] === '4') {
+        total += 4;
+      } else if (currentCard[x] === '3') {
+        total += 3;
+      } else if (currentCard[x] === '2') {
+        total += 2;
+      }
+      if (total > winnerTotal) {
+        winnerTotal = total;
+        currentWinner = currentPlayer.name;
+      }
+    }
+  }
+}
+
+console.log('The winner is: ', currentWinner);
