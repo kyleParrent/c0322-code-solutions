64777cea2983a2dfcdf70ad656beeeaeda84ed1e
Kyle Parrent <kppp433@gmail.com>
2022-05-24 17:37:50 +0000

    Completed express-static.

---
+++ express-static/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const joinedPath = path.join(__dirname, 'public');
+console.log(joinedPath);
+
+const exStatic = express.static('public');
+
+app.use(exStatic);
+
+app.listen(3000, () => {
+  console.log('Server is watching on port 3000');
+});
