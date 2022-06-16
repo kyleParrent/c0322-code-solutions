9091c518ef5d7401f3d4c33d0099b70c97c888c5
Kyle Parrent <kppp433@gmail.com>
2022-06-16 20:34:07 +0000

    Completed data-structures-linked-lists.

--- data-structures-linked-lists/append.js
+++ data-structures-linked-lists/append.js
@@ -2,5 +2,9 @@
 /* exported append */
 
 function append(list, value) {
-
+  while (list.next !== null) {
+    list = list.next;
+  }
+  const newValue = new LinkedList(value);
+  list.next = newValue;
 }
--- data-structures-linked-lists/remove-tail.js
+++ data-structures-linked-lists/remove-tail.js
@@ -1,5 +1,13 @@
 /* exported removeTail */
 
 function removeTail(list) {
-
+  if (list.next === null) {
+    return;
+  }
+  let newList = list;
+  while (newList.next.next !== null) {
+    newList = newList.next;
+  }
+  newList.next = null;
+  return newList;
 }
